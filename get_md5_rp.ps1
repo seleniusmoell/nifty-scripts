5b6b059070db25e3f8f09748e7b0a3a9e6319078
tosemo <tove.selenius.moell@paynova.com>
2021-12-10 14:39:10 +0100

    update

--- get_md5_rp.ps1
+++ get_md5_rp.ps1
@@ -1,7 +1,9 @@
 $time = Get-Date
-$string = "377f159b-a908-4245-a2b9-ac9300855722" + "($time)"
+$order_nr = Read-Host -Prompt 'Enter the order number'
+echo "Order number is: $order_nr"
+$string = "($order_nr)" + "($time)"
 $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
 $utf8 = New-Object -TypeName System.Text.UTF8Encoding
 $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($string)))
-echo "Time is: " + "($time)"
-echo "Hash is: " + "($hash)"
+echo "Time is: $time"
+echo "Hash is: $hash"
